bf08c88831edd1ce2a2efec3aadb38d709f76928
Michael Allman <mike@Michaels-MacBook-Pro.local>
2019-04-14 15:55:36 +0100

    added basic counter

--- src/App.js
+++ src/App.js
@@ -2,27 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Welcome(props) {
+  return <h1>Hello {props.name}</h1>;
+}
+
+function Button(props) {
+  return <button className="Button" onClick={props.onClick}> {props.buttonName} </button>
+}
+
+function ButtonStatus(props) {
+  return <footer> Button clicked {props.n} times </footer>
+}
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      clicks: 0
+    }
+  }
+
+  buttonClicked = () => {
+    const n = this.state.clicks
+    this.setState({
+      clicks: n + 1
+    })
+    if (this.state.clicks === 10) {alert("Ricky is a cunt.")}
+  }
+
+  reset = () => {
+    this.setState({
+      clicks: 0
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img className="App-logo" src={logo}/>
+        <Welcome name="Mike" />
+        <Button buttonName="Click me!" onClick={this.buttonClicked} />
+        <Button buttonName="Reset" onClick={this.reset} />
+        <ButtonStatus n={this.state.clicks} />
       </div>
-    );
+      
+    )
   }
+
 }
+
 
 export default App;
