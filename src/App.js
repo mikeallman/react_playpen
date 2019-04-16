87e93781c8798ba4024b5959f37934eae5090467
michaela <michael.allman@dunnhumby.com>
2019-04-16 16:43:46 +0100

    added footer and played around with css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const name = "Mike Allman"
+const email = "mike@allman.io"
+const github = "https://github.com/mikeallman/react_playpen"
+
 function Welcome(props) {
-  return <h1>{props.name} is learning react</h1>;
+  return <h1>{props.name} is learning react!</h1>;
 }
 
 function Button(props) {
@@ -11,7 +15,7 @@ function Button(props) {
 }
 
 function ButtonStatus(props) {
-  return <footer> Button clicked {props.n} times </footer>
+  return <div className="ButtonStatus"> Button clicked {props.n} times </div>
 }
 
 class App extends Component {
@@ -39,13 +43,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <img className="App-logo" src={logo} alt="React Logo"/>
+        <div>
+          <img className="App-logo" src={logo} alt="React Logo"/>
+        </div>
         <Welcome name="Mike" />
         <Button buttonName="Click me!" onClick={this.buttonClicked} />
         <Button buttonName="Reset" onClick={this.reset} />
         <ButtonStatus n={this.state.clicks} />
+        <footer>
+          <a className="FooterAnchor" href={`mailto:${email}`}>Hacked up by {name}</a>
+          <a className="FooterAnchor" href={github} target="_blank">View on GitHub</a>
+        </footer>
       </div>
-      
     )
   }
 
